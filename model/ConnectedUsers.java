2fa026a774da2665f904dac460c7be75c20515df
Luis Rodriguez-Vallejo <luis.rodriguez-vallejo.ext@enac.fr>
2017-05-07 15:43:26 +0200

    modif CheckConnectedUsers + tests

--- model/ConnectedUsers.java
+++ model/ConnectedUsers.java
@@ -20,7 +20,7 @@ public class ConnectedUsers implements ListSelectionListener {
 	
 	private DefaultListModel<User> listUsers = new DefaultListModel<User>();
 	
-	public ConnectedUsers(){   //Private constructor to implement Singleton design pattern
+	public ConnectedUsers(){   
 	}
 		
 	public boolean containsUser(User user){
@@ -34,38 +34,8 @@ public class ConnectedUsers implements ListSelectionListener {
 	
 	
 	public void valueChanged(ListSelectionEvent arg0) {
-		System.out.println("to");
+		
 	}
 	
-//	public static void main(String[] args) {
-//		ConnectedUsers s = new ConnectedUsers();
-//		User u1=null;
-//		User u2 =null;
-//		try {
-//			u1 = new User("h1", InetAddress.getLocalHost(), 4562, typeConnect.CONNECTED);
-//			u2 = new User("h2", InetAddress.getLocalHost(), 4542, typeConnect.CONNECTED);
-//		} catch (UnknownHostException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		UserDate UD1 = new UserDate(u1, new Date());
-//		UserDate UD2 = new UserDate(u2, new Date());
-//	
-//		s.getListUsers().addElement(UD1);
-//		s.getListUsers().addElement(UD2);
-//		UserDate user = s.getUser(UD1);
-//		System.out.println("user : " + user.toString());
-//		System.out.println(s.toString());
-//		try {
-//			Thread.sleep(2000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		s.setDate(UD1);
-//		System.out.println("user : " + user.toString());
-//		System.out.println(s.toString());
-//		UpdateConnectedUsers.getInstance().updateDate(UD1);
-//	}
+
 }
--- tests/ConnectedUsersTest.java
+++ tests/ConnectedUsersTest.java
@@ -1,14 +1,13 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import model.ConnectedUsers;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import communication.User;
@@ -20,15 +19,34 @@ public class ConnectedUsersTest {
 	@Test
 	public void testContainsUser() {
 		ConnectedUsers con = new ConnectedUsers();
-//		User us1= null;
-//		try {
-//			us1 = new User("lachaume", InetAddress.getLocalHost(), 1324, typeConnect.CONNECTED);
-//		} catch (UnknownHostException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		con.getListUsers().addElement(us1);
-//		assertEquals(con.containsUser(us1), true);
+		
+		User us1= null;
+		try {
+			us1 = new User("lachaume", InetAddress.getLocalHost(), 1324, typeConnect.CONNECTED);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		con.getListUsers().addElement(us1);
+		assertEquals(con.containsUser(us1), true);
+	}
+	
+	@Test
+	public void testContainsUknonwUser() {
+		ConnectedUsers con = new ConnectedUsers();
+		InetAddress addLoc= null;
+		try {
+			addLoc = InetAddress.getLocalHost();
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		User us1 = new User("lachaume", addLoc, 1224, typeConnect.CONNECTED);
+		User us2 = new User("thibaut", addLoc, 1225, typeConnect.CONNECTED);
+		
+		con.getListUsers().addElement(us1);
+		//con.getListUsers().addElement(us2);
+		assertEquals(con.containsUser(us2),false);
 	}
 
 }
